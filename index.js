f5e184d7b8209026fd0f73bdbfea046599ce72c9
Symion Edwards <symion.edwards@gmail.com>
2020-05-11 12:46:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  const lotteryNumbers = [2, 4, 5, 2]
+  lotteryNumbers.forEach(callback)
+  return lotteryNumbers
+}
